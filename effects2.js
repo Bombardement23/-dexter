ff53972a492352a8c6477aa4c6916eb8dc8e5c13
Melino <161760301+Bombardement23@users.noreply.github.com>
2025-10-01 13:40:04 +0200

    Update effects2.js

--- effects2.js
+++ effects2.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const imgSrc = "https://lanyard.cnrad.dev/api/754225633055211561";
+    const imgSrc = "https://lanyard.cnrad.dev/api/1415320040592642091";
     const imgElement = document.getElementById('rpc');
     imgElement.src = imgSrc;
 });
